5cb1583c7b9d49bc58103b716172d72532161e80
Nahidur Rahaman <103555932+NahidurRahaman@users.noreply.github.com>
2024-10-12 11:50:51 +0600

    Add files via upload

---
+++ basic_string.py
@@ -0,0 +1,13 @@
+data="Nahidur Rahaman"
+s=type(data)
+print(s)
+l=len(data)
+print(l)
+import sys
+a=sys.getsizeof(data)
+print(a)
+c=data.count('a')
+print(c)
+sub='am'
+su=data.count(sub)
+print(su)

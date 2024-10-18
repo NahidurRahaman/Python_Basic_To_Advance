7eb4cd9f5e2762673c8fe65307627e46eaa5da0c
Nahidur Rahaman <103555932+NahidurRahaman@users.noreply.github.com>
2024-10-18 22:27:19 +0600

    Add files via upload

---
+++ operator_1.py
@@ -0,0 +1,45 @@
+# Assinging the variables
+x = 15
+y = 4
+z = 3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y)
+print(x//y)
+import math
+math.floor(x/y)
+math.ceil(x/y)
+#Python Identity Operators
+x = 10
+y = 10
+print(x is y)
+print(x == y)
+x = 10000017
+y = x
+z = 10000017
+print(x is y)
+print(x is z)
+print(id(x))
+print(id(y))
+print(id(z))
+#Membership Operator
+
+list1 = [1,2,3,4,5,6,7]
+
+print(1 in list1)
+print(10 not in list1)
+st1 = 'Learn Data Science'
+print('data' in st1)
+#Python Bitwise Operators
+
+x = 10
+y = 5
+
+print(x |y)
+print(x & y)
+x = 10
+x += 4
+
+

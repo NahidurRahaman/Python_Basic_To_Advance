3d47fbfe81d22bdf461f4dd690c3c6f52761af45
Nahidur Rahaman <103555932+NahidurRahaman@users.noreply.github.com>
2025-06-29 17:28:28 +0600

    Add files via upload

--- 01_problem1.py
+++ 01_problem1.py
@@ -1,21 +1,21 @@
-class TwoDVector:
-    def __init__(self, i, j):
-        self.i = i
-        self.j = j
-    
-    def show(self):
-        print(f"The vector is {self.i}i + {self.j}j ")
-        
 
-class ThreeDVector(TwoDVector):
-    def __init__(self, i, j, k):
-        super().__init__(i, j)
-        self.k = k
+try:
+    with open("1.txt", "r") as f:
+        print(f.read())
+except Exception as e:
+    print(e)
 
-    def show(self):
-        print(f"The vector is {self.i}i + {self.j}j + {self.k}k")
+try:
+    with open("2.txt", "r") as f:
+        print(f.read())
+except Exception as e:
+    print(e)
 
-a = TwoDVector(1, 2)
-a.show()
-b = ThreeDVector(5, 2, 3)
-b.show()
+try:
+    with open("3.txt", "r") as f:
+        print(f.read())
+except Exception as e:
+    print(e)
+
+
+print("Thank You!")
